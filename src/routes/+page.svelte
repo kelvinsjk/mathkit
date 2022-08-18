995c2ecfb7c983ba1e0d03ae5660b1acff51f4a1
Kelvin Soh <kelvinsoh@pop-os.localdomain>
2022-08-19 01:39:01 +0800

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import {math} from 'mathlifier';
+  const x = math('x');
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{@html x} x
